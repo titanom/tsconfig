64b6a8d9340893421ec7367fc54b547a8be8d934
Jannis Morgenstern <nyarthan@proton.me>
2023-03-18 23:43:55 +0100

    fix: exit with non-zero exit code if at least one config is invalid

--- test.js
+++ test.js
@@ -5,12 +5,17 @@ const fs = require('node:fs');
 
 const files = fs.readdirSync(__dirname).filter((filename) => filename.includes('tsconfig'));
 
+let failed = 0;
+
 for (const file of files) {
   console.log(`------ Running TypeScript compiler with file: ${file}. ------\n`);
   try {
     console.log(execSync(`pnpm tsc -p ${file}`).toString());
   } catch (error) {
+    failed++;
     const stdout = error.stdout.toString();
     console.log(stdout);
   }
 }
+
+if (failed) process.exit(1);
